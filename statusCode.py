a94869ac6aaf808af9404f1957b33efb62555402
Jacky Huang <jacky_huang56@hotmail.com>
2021-01-11 19:56:35 -0700

    finished

--- statusCode.py
+++ statusCode.py
@@ -1,4 +1,10 @@
 import requests
 
-r = requests.get('http://google.com', auth=('user', 'pass'))
-print (r.status_code)
+# print(requests.__version__)
+
+# r = requests.get('http://google.com', auth=('user', 'pass'))
+# print (r.status_code)
+
+r = requests.get('https://raw.githubusercontent.com/JackyH56/CMPUT404LAB1/main/statusCode.py')
+
+print(r.text)
